6713afa058dc06ddd25c490c561ed83b19b02033
Gauthier Defrance <135462312+GauthierDefrance@users.noreply.github.com>
2026-03-17 11:35:32 +0100

    Update MainCli.cpp

--- src/cli/MainCli.cpp
+++ src/cli/MainCli.cpp
@@ -1,3 +1,54 @@
 #include "MainCli.h"
+#include "stdio.h"
 
 
+MainCli& MainCli::getInstance() {
+        static MainCli instance;
+        return instance;
+}
+
+MainCli::MainCli() {
+
+}
+
+MainCli::~MainCli() {
+        delete f;
+}
+
+
+// Etat du CLI
+void MainCli::run(int argc, char** argv) {
+
+}
+
+//Fonctions d'affichages
+void MainCli::showHelp() {
+
+
+}
+void MainCli::printBuiltinAlgorithm() {
+
+}
+
+//static void printAlgorithmResultat(Resultat resultat);
+
+//Méthodes utilitaire
+void MainCli::loadFile(char *filepath) {
+
+}
+
+
+void MainCli::printCurrentFileName() {
+
+}
+
+
+void MainCli::selectAlgorithm(SchedulingAlgorithm algorithm) {
+        
+
+}
+
+
+void MainCli::loadFileAndSelectAlgorithm(char *filepath, SchedulingAlgorithm algorithm) {
+
+}
