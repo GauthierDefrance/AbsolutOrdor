1fd0c04c982f52c899c6a8b073701abc47aefc4d
GauthierDefrance <defrance.gauthier@gmail.com>
2026-03-31 00:47:53 +0200

    Update ConsoleStructPrinter.h

--- src/cli/ConsoleStructPrinter.h
+++ src/cli/ConsoleStructPrinter.h
@@ -5,15 +5,36 @@
 #include <string>
 
 extern "C" {
-#include "data/struct/liste.h"
-#include "data/struct/liste_tq.h"
-#include "data/struct/processus.h"
-#include "data/struct/ExecutionTimeline.h"
-#include "process/utilities/TimelineStatsCalculator.h"
+    #include "data/struct/liste.h"
+    #include "data/struct/liste_tq.h"
+    #include "data/struct/processus.h"
+    #include "data/struct/ExecutionTimeline.h"
+    #include "process/utilities/TimelineStatsCalculator.h"
 }
 
-
+/**
+ * Affiche une ListeTQ de Processus
+ * @param liste
+ */
 void afficherCSVCharge(ListeTQ liste);
+
+
+/**
+ * Affiche la Timeline
+ * @param tl
+ */
 void afficherTimeline(const ExecutionTimeline *tl);
+
+
+/**
+ * Affiche la Timeline avec un décalage pour que tout les éléments soient alignés
+ * @param tl
+ */
 void afficherTimelineAvecDecalage(const ExecutionTimeline *tl);
+
+
+/**
+ * Affiche le résultat des calculs des stats de la Timeline
+ * @param tl
+ */
 void afficherStatsTimeline(const ExecutionTimeline *tl);
